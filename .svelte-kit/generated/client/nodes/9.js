a34b5778d5dfd66624cc193092cb2338c94e852b
Michael Hegner <hegner123@gmail.com>
2024-07-18 15:21:57 -0400

    updated .gitignore

--- .svelte-kit/generated/client/nodes/9.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/timers/[slug]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/timers/[slug]/+page.svelte";
